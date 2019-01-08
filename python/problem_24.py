e280b552edb52e25f4b3563eb604258a87233a14
Tan, Jude (RBI-UK) <jude.tan@accuity.com>
2019-01-08 22:58:55 +0800

    added in problem 24 not done

---
+++ python/problem_24.py
@@ -0,0 +1,17 @@
+# coding=utf-8
+# Solution to problem 22
+# Problem Statement: Lexicographic permutations
+#
+# A permutation is an ordered arrangement of objects. For example,
+# 3124 is one possible permutation of the digits 1, 2, 3 and 4.
+# If all of the permutations are listed numerically or alphabetically,
+# we call it lexicographic order. The lexicographic permutations of 0, 1 and 2 are:
+# 012   021   102   120   201   210
+# What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
+#
+#
+
+
+
+if __name__ == "__main__":
+    print (28123)
